5d69e64ad543cc5dbe1093d74b8af81f3e396cbf
leon <leon.sturm2@gmail.com>
2025-10-24 19:13:08 +0200

    seperated code into modules -> main and studentski-servis

---
+++ AIRFLOW/dags/studentski_servis.py
@@ -0,0 +1,26 @@
+
+def scrap_studentski_servis(soup, inserted_func):
+    """
+        soup:           BeautifulSoup object of the webpage
+        insert_func:    function to insert a record into Firebase
+    """
+        
+    articles = soup.body.main.find_all("article")
+
+    for article in articles:
+        article_content = article.div.div
+        titles = article_content.find_all('h5')
+
+        if len(titles) == 1:
+            title = f"{titles[0].get_text(strip=True)}"
+        else:    
+            title = f"{titles[0].get_text(strip=True)} : {titles[1].get_text(strip=True)}"
+        description = article_content.find_all('p')[-1].string
+        location = article_content.find_all('p')[1].get_text(separator=" ", strip=True)  # the <p> tag
+        #print(title)
+        #print(location)
+        #print(description)
+        #print("\n")
+
+        # Insert into DB using the function passed from the controller
+        inserted_func(title, location, description)
--- AIRFLOW/dags/webscrapping.py
+++ AIRFLOW/dags/webscrapping.py
@@ -8,27 +8,22 @@ import firebase_admin
 from firebase_admin import db, credentials
 from datetime import datetime, timedelta
 
-URI_studentski_servis = " \
-https://www.studentski-servis.com/studenti/prosta-dela?kljb=&page=1&isci=1&sort=&dd1=1&dm1s=1 \
-&regija%5B%5D=ljubljana-z-okolico&regija%5B%5D=domzale-kamnik&skD%5B%5D=004&skD%5B%5D=A832&sk \
-D%5B%5D=A210&skD%5B%5D=A055&skD%5B%5D=A078&skD%5B%5D=A090&skD%5B%5D=A095&hourlyratefrom=6.32&hourlyrateto=36&hourly_rate=6.32%3B41 \
-"
+from studentski_servis import scrap_studentski_servis
+
+
+# --- Firebase setup ---
 cred = credentials.Certificate("db-credentials.json")
 firebase_admin.initialize_app(cred, {
     "databaseURL": "https://webscrapping-student-default-rtdb.europe-west1.firebasedatabase.app/"
 })
 ref = db.reference("/")
 
+# --- Dates ---
 today_date = datetime.today().strftime("%Y-%m-%d")
 yesterday_date = (datetime.today() - timedelta(days=1))
 
-# STUDENTKSI SERVIS
 
-webpage = requests.get(URI_studentski_servis).text
-soup = BeautifulSoup(webpage, 'html.parser')
-
-articles = soup.body.main.find_all("article")
-
+# --- SHared functions ---
 
 def push_to_db(title,location,description,today_date):
     ref.push({
@@ -39,30 +34,17 @@ def push_to_db(title,location,description,today_date):
     })
 
 
-for article in articles:
-    article_content = article.div.div
-    titles = article_content.find_all('h5')
-
-    if len(titles) == 1:
-        title = f"{titles[0].get_text(strip=True)}"
-    else:    
-        title = f"{titles[0].get_text(strip=True)} : {titles[1].get_text(strip=True)}"
-    description = article_content.find_all('p')[-1].string
-    location = article_content.find_all('p')[1].get_text(separator=" ", strip=True)  # the <p> tag
-    #print(title)
-    #print(location)
-    #print(description)
-    #print("\n")
 
     # Send webscrapped data from STUDETNSKI SERVIS to firebase
 
+def insert_to_db_if_new_record(title,location,description):
     # Get newest 10 records from DB
     records = ref.order_by_key().limit_to_last(10).get()
     
     # push straight away if no prior records in DB
     if not records:
         push_to_db(title,location,description,today_date)
-        break
+        return
 
     # loop thru sorted records
     for key in sorted(records.keys(), reverse=True):
@@ -79,3 +61,34 @@ for article in articles:
                 # The current record exists in DB, skip it
                 print(f"Record {title[0:13]} exists in DB")
                 break
+
+
+# STUDENTKSI SERVIS ////////////////////////////////////////////////////////////////
+
+URI_studentski_servis = " \
+https://www.studentski-servis.com/studenti/prosta-dela?kljb=&page=1&isci=1&sort=&dd1=1&dm1s=1 \
+&regija%5B%5D=ljubljana-z-okolico&regija%5B%5D=domzale-kamnik&skD%5B%5D=004&skD%5B%5D=A832&sk \
+D%5B%5D=A210&skD%5B%5D=A055&skD%5B%5D=A078&skD%5B%5D=A090&skD%5B%5D=A095&hourlyratefrom=6.32&hourlyrateto=36&hourly_rate=6.32%3B41 \
+"
+
+webpage = requests.get(URI_studentski_servis).text
+soup = BeautifulSoup(webpage, 'html.parser')
+
+scrap_studentski_servis(soup, insert_to_db_if_new_record)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    print("main executed")
+
